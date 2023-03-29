ee4ee9fcc108b93f88bbce581ad0163546591c8c
Timothy DeBerry <timothy.c.deberry2.mil@army.mil>
2023-03-29 15:29:53 +0000

    Initial commit: Setup.

---
+++ include/Cthreading.h
@@ -0,0 +1,13 @@
+/** @file Cthreading.h
+    * 
+    * @brief A description of the module's purpose. *****CHANGE ME*****
+    *
+    */
+    
+    #ifndef CTHREADING\_H
+    #define CTHREADING\_H
+
+
+
+
+    #endif
---
+++ src/Cthreading.c
@@ -0,0 +1,11 @@
+/** @file Cthreading.h
+    * 
+    * @brief A description of the module's purpose. *****CHANGE ME*****
+    *
+    */
+
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+
+    #include ../include/Cthreading.h
